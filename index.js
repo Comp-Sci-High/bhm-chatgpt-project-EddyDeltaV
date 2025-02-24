08b22401d1560258224fc0d0af332d525720c81b
EddyDeltaV <Eduardo.santos25@compscihigh.org>
2025-02-24 19:20:06 +0000

    finished -images

--- index.js
+++ index.js
@@ -30,6 +30,12 @@ app.use((req, res, next) => {
   app.get("/",(req,res)=>{
     res.sendFile(__dirname + "/public/index.html")
   })
+  app.get("/about",(req,res)=>{
+    res.sendFile(__dirname + "/public/about.html")
+  })
+  app.get("/contact",(req,res)=>{
+    res.sendFile(__dirname + "/public/contact.html")
+  })
   
   app.listen(3000, (req, res) => {
     console.log("hey")
